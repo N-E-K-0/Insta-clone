84c994b2d2994eea92d8431bc646bfab5ae10dd6
Mohammed Samiul Islam <39405587+N-E-K-0@users.noreply.github.com>
2021-04-06 13:12:42 +0600

    simple middleware

--- server/app.js
+++ server/app.js
@@ -3,7 +3,20 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = (req,res,next) =>{
+    console.log("middleware executed!")
+    next()
+}
+
+// app.use(customMiddleware)
+
 app.get('/',(req,res)=> {
+    console.log("home")
+    res.send("Hello world")
+})
+
+app.get('/about',customMiddleware,(req,res)=> {
+    console.log("about")
     res.send("Hello world")
 })
 
