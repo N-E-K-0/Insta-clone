331f6e60336f74094d85b6ec2d299ad415e7fc1a
Mohammed Samiul Islam <39405587+N-E-K-0@users.noreply.github.com>
2021-04-06 13:37:56 +0600

    setup the database

--- server/app.js
+++ server/app.js
@@ -1,26 +1,21 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 5000
+const { MONGOURI } = require('./keys')
 
-const customMiddleware = (req,res,next) =>{
-    console.log("middleware executed!")
-    next()
-}
-
-// app.use(customMiddleware)
-
-app.get('/',(req,res)=> {
-    console.log("home")
-    res.send("Hello world")
+mongoose.connect(MONGOURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.on('connected',()=>{
+  console.log('connected to mongoDB!')
+})
+mongoose.connection.on('error',(err)=>{
+  console.log('error connecting to mongoDB!', err)
 })
 
-app.get('/about',customMiddleware,(req,res)=> {
-    console.log("about")
-    res.send("Hello world")
-})
 
 app.listen(PORT, ()=>{
-    console.log("server is running on", PORT)
+  console.log("server is running on", PORT)
 })
-
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI : 'mongodb+srv://Samiul:pUqjsD8HeHU4cgYM@cluster0.voban.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
